d4348c50fd0cc57e98bd8e7dbe2402a48185458b
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-08 10:24:28 -0300

    inclusão de bolhas

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,5 +1,5 @@
-const TEMPO_MINIMO = 1000;    // 1s em milissegundos
-const TEMPO_VARIAVEL = 3000;  // 3s
+const TEMPO_MINIMO = 200;     
+const TEMPO_VARIAVEL = 300;   
 const DURACAO = 5000;         // 5s
 const bolhaEl = carregaUmaImagem('images/bolha.png');
 
